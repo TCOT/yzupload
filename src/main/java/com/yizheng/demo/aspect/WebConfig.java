6ecd2a88fd31f50e126ee0bc1dae65107b27ca5c
Yi Zheng <971302145@qq.com>
2017-11-02 18:16:46 +0800

    更新

--- src/main/java/com/yizheng/demo/aspect/WebConfig.java
+++ src/main/java/com/yizheng/demo/aspect/WebConfig.java
@@ -4,16 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-//package com.yizheng.demo.aspect;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//public class WebConfig extends WebMvcConfigurerAdapter {
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/avatar/**").addResourceLocations("classpath:/avatar/");
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/avatar/**").addResourceLocations("classpath:/avatar/");
+    }
+}
